996723fb3417612d4572875f9e418d9bf9d95aac
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-02-16 16:41:29 +0545

    Added Email verification for users

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -4,7 +4,7 @@ import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { toast } from 'react-toastify'
-
+import { useEffect } from 'react'
 
 const EmailVerify = () => {
   axios.defaults.withCredentials=true
@@ -40,14 +40,21 @@ const EmailVerify = () => {
         getUserData()
         navigate('/')
       }
+      else{
+        toast.error(data.message)
+      }
     } catch (error) {
       toast.error(error.message)
     }
   }
+
+  useEffect(()=>{
+    isLoggedin && userData && userData.isAccountVerified && navigate('/')
+  },[isLoggedin,userData])
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
-          <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+          <form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
             <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verification OTP</h1>
             <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email.</p>
             <div className='flex justify-between mb-8' onPaste={handlePaste}>
