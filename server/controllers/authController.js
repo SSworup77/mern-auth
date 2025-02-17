0955aba05dda9cfe9a504cd1b64689389c026e82
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-02-17 16:41:37 +0545

    Integrated Email templates for email verification and password reset

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 import transporter from '../config/nodemailer.js';
+import { EMAIL_VERIFY_TEMPLATE,PASSWORD_RESET_TEMPLATE } from '../config/emailTemplates.js';
 
 export const register = async (req,res)=>{
     const {name,email,password} = req.body;
@@ -108,7 +109,8 @@ export const sendVerifyOtp=async(req,res)=>{
             from:process.env.SENDER_EMAIL,
             to: user.email,
             subject: 'Account Verification OTP',
-            text: `Your OTP is ${otp}.Verify your account using this OTP.`
+            // text: `Your OTP is ${otp}.Verify your account using this OTP.`,
+            html:EMAIL_VERIFY_TEMPLATE.replace('{{otp}}',otp).replace('{{email}}',user.email)
         }
         await transporter.sendMail(mailOptions);
         res.json({
@@ -175,7 +177,8 @@ export const sendResetOtp=async(req,res)=>{
             from:process.env.SENDER_EMAIL,
             to: user.email,
             subject: 'Password reset OTP',
-            text: `Your OTP for resetting your password is ${otp}.Reset you password using this OTP.`
+            // text: `Your OTP for resetting your password is ${otp}.Reset you password using this OTP.`,
+            html:PASSWORD_RESET_TEMPLATE.replace('{{otp}}',otp).replace('{{email}}',user.email)
         };
         await transporter.sendMail(mailOptions);
         return res.json({success:true,message:'Reset OTP sent to your email'});
