4ef554e9925e5ba4cdb4ab697ef1948d2d048615
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-05-04 21:27:48 +0545

    Cleaning code

--- server/server.js
+++ server/server.js
@@ -7,18 +7,18 @@ import authRouter from "./routes/authRoutes.js"
 import userRouter from "./routes/userRoutes.js";
 
 
-const app =express();
-const port=process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins= ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173']
 app.use(express.json());
-app.use(cors({origin:allowedOrigins, credentials:true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
 
 //API endpoints
-app.get('/',(req,res)=>res.send("API working"));
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
+app.get('/', (req, res) => res.send("API working"));
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
-app.listen(port,()=>console.log(`Server started on PORT:${port}`));
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
